a218d36d395784590254be737b2526160be71871
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2022-01-17 17:51:52 +0300

    setup: Inıtialize the project

---
+++ src/redux/books/books.js
@@ -0,0 +1,16 @@
+import Book from "../../components/book";
+
+
+export default function functionName(){
+
+    return (
+      <ul>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+      </ul>
+    )
+
+}
