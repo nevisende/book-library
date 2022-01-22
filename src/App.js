fb47b674a564a584af53b3beac56155f66b3ef24
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2022-01-22 18:15:24 +0300

    feat: Style components

--- src/App.js
+++ src/App.js
@@ -6,24 +6,28 @@ import {
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
-
+import personSvg from './assets/person.svg';
 import Books from './page/BookPage';
 import Categories from './page/CategoryPage';
-import logo from './assets/logo.svg';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <div>
+      <div className="p-0 m-0 min-h-full">
         <BrowserRouter>
-          <ul>
-            <img src={logo} alt="logo" />
-            <li>
-              <Link to="/">Books</Link>
-            </li>
-            <li>
-              <Link to="categories">Categories</Link>
-            </li>
+          <ul className="flex px-20 py-4 flex-row place-items-center justify-between w-full">
+            <div className="flex flex-row place-items-center gap-10">
+              <h1 className="text-3xl font-bold text-[#0290ff]">Book Library</h1>
+              <li>
+                <Link className="text-gray-700 font-semibold" to="/">BOOKS</Link>
+              </li>
+              <li>
+                <Link className="text-gray-400 font-semibold" to="categories">CATEGORIES</Link>
+              </li>
+            </div>
+            <div className="bg-gray-100 p-4 rounded-full">
+              <img src={personSvg} className="w-5 fill-blue-700" alt="person icon" />
+            </div>
           </ul>
           <Routes>
             <Route path="categories" element={<Categories />} />
--- src/components/book.js
+++ src/components/book.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/books';
 import BookAPI from '../services/bookAPI';
+import progressImage from '../assets/circlee.png';
 
 export default function Book({
   id,
@@ -18,10 +19,32 @@ export default function Book({
   }
   return (
     <>
-      <span>{category}</span>
-      <h3>{title}</h3>
-      <span>{id}</span>
-      <button type="button" onClick={deleteBook}>Remove</button>
+      <div className="flex-1 flex flex-col justify-between">
+        <div className="mb-4">
+          <span className="font-bold text-gray-500 text-sm">{category}</span>
+          <h3 className="font-bold text-xl p-0">{title}</h3>
+          <span className="text-blue-400 text-sm">{id}</span>
+        </div>
+        <div className="text-blue-400">
+          <button className="border-r-2 pr-6" type="button">Comments</button>
+          <button className="border-r-2 pr-6 pl-6" type="button" onClick={deleteBook}>Remove</button>
+          <button className="pl-6" type="button">Edit</button>
+        </div>
+      </div>
+      <div className="flex-1 flex place-items-center">
+        <div className="flex-1  border-r-2 flex place-items-center">
+          <img src={progressImage} className="h-20 mr-4" alt="progress bar" />
+          <div>
+            <h4 className="text-3xl font-semibold">75%</h4>
+            <span className="text-gray-400">Completed</span>
+          </div>
+        </div>
+        <div className="flex-1 pl-10">
+          <span className="text-gray-400 block mb-2">CURRENT CHAPTER</span>
+          <span className="text-black block mb-2">Chapter 17</span>
+          <button className="py-1 px-10 flex-none bg-[#0290ff] text-gray-300 rounded" type="submit">UPDATE PROGRESS</button>
+        </div>
+      </div>
     </>
   );
 }
